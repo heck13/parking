a2415437e0c0a73cafd1eabe743e83cad82b0ec0
personAM <anatolschiopu0@gmail.com>
2018-11-13 19:54:14 +0200

    other commit

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,22 +1,23 @@
 package com.example.demo.controllers;
 
 
-import com.example.demo.model.MyModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@Controller returneaza pagini web
+@RestController // returneaza numai text
 public class MainController {
 
     @Autowired
     private MyModel model;
 
 
-    @RequestMapping(value = "/anatol", method = RequestMethod.GET)
-    public String anatol(){
+    @RequestMapping(value = "/anatolie", method = RequestMethod.GET)
+    public String anatolie(){
         model.setName("Hello Anatol!!!");
+        model.setAge(30);
 
         return model.toString();
     }
